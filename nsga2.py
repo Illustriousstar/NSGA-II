c30afe65cea7f48e95de6694e74a7ade58afc5c5
Illustriousstar <vaguetwm@gmail.com>
2023-05-08 16:21:37 +0800

    适配新数据

--- nsga2.py
+++ nsga2.py
@@ -20,7 +20,7 @@ num_population = 2000 # 种群规模
 prob_crossover = 0.9 # 交叉概率
 prob_mutate = 0.1 # 变异概率
 
-airline_transport_num=pd.read_csv('test.csv',header=None)
+airline_transport_num=pd.read_csv('airline_sum57.csv')
 airline_people_raw = pd.read_excel("高峰小时旅客运输量.xlsx", header=None)
 airline_union_raw=pd.read_excel('所属航系.xlsx')
 airline_building_max=pd.read_excel('航站楼最大客流量.xlsx').values[:,2].squeeze().astype(int)
@@ -28,7 +28,8 @@ num_building = len(airline_building_max) # 航站楼数量
 num_airline = len(airline_people_raw) # 航空公司数量
 
 # 航空公司之间转运的人流量
-airline2num=dict(zip(airline_transport_num[0],airline_transport_num[1]))
+airline_transport_num["进港_出港"] = airline_transport_num["进港航司"] + "_" + airline_transport_num["出港航司"]
+airline2num=dict(zip(airline_transport_num["进港_出港"],airline_transport_num["今年旅客人数"]))
 
 # 航系对应的航空公司
 airline_union=airline_union_raw.values.transpose()
@@ -45,11 +46,12 @@ for i, count in enumerate(airline_union_count):
     cumsum += count
 airline_union2d = (airline_union_id.reshape(-1, 1) == airline_union_id.reshape(1, -1)).astype(int)
 
+# 按照（航系确定的）航空公司顺序计算转运人数
 airline_transport_num = []
 for i in range(num_airline):
     airline_transport_num.append([])
     for j in range(num_airline):
-        airline_transport_num[i].append(airline2num.get(airline_list[i]+'_'+airline_list[j],0) / 2.)
+        airline_transport_num[i].append(airline2num.get(airline_list[i]+'_'+airline_list[j],0))
 airline_transport_num = np.array(airline_transport_num,dtype=int)
 
 # 航空公司高峰人流量
